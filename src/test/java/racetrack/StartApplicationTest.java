eb3d86f26928c985448f49865850d6931567c8bf
AlexanderTereshkin <nnovcity@mail.ru>
2019-11-25 15:03:32 +0300

    Test of loading context by Spring.

---
+++ src/test/java/racetrack/StartApplicationTest.java
@@ -0,0 +1,16 @@
+package racetrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
