511045b98979bd5a05932838de6e0421eecacd94
Александр <nnovcity@mail.ru>
2019-11-26 23:36:58 +0300

    Update.

--- src/main/java/racetrack/repositories/CarRepository.java
+++ src/main/java/racetrack/repositories/CarRepository.java
@@ -1,9 +1,7 @@
 package racetrack.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import racetrack.domain.Car;
 
-@Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
