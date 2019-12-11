be8564d3bd0cca51aba97e05cbd230bfef2af54c
AlexanderTereshkin <nnovcity@mail.ru>
2019-12-11 17:14:18 +0300

    Update entities.

--- src/main/java/com/company/racetrack/security/SecurityConfig.java
+++ src/main/java/com/company/racetrack/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.company.racetrack.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,8 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import sun.security.util.ManifestDigester;
 
 import javax.sql.DataSource;
 
